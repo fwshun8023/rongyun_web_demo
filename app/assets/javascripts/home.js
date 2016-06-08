f572530833370df5d2f04228202f59582ee24fa5
fanwei <fanwei@netfarmer.com.cn>
2016-06-08 17:06:30 +0800

    js fix

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -3,7 +3,7 @@ demo.controller("main", ["$scope", "WebIMWidget", function($scope,WebIMWidget) {
   angular.element(document).ready(function() {
     WebIMWidget.init({
       appkey: "c9kqb3rdklwcj",
-      token: "ngIjxKIKtcZbrEJcYFsjkJd0d70yTO5eXlGYpPD6C2HBW4YF26lf/8emO0ff8n/hLipiFxpV/YvB/n+h21FKXA==",
+      token: "5FYwSlFu7N5kIDkBnDvvHwRGRBzxPWlUi2kesloL0c+iHzTDQyurYf2/cDS7CRZXFUDjx4X+BOE=",
       style: {
         width: 600,
         positionFixed: true,
@@ -13,7 +13,7 @@ demo.controller("main", ["$scope", "WebIMWidget", function($scope,WebIMWidget) {
       displayConversationList: true,
       conversationListPosition: WebIMWidget.EnumConversationListPosition.left,
       onSuccess: function(id){
-        WebIMWidget.setConversation(1, "f3", "李四3");
+        WebIMWidget.setConversation(1, "f1", "张三1");
         console.log(id);
       },
       onError: function(error){
@@ -22,17 +22,28 @@ demo.controller("main", ["$scope", "WebIMWidget", function($scope,WebIMWidget) {
     });
 
     WebIMWidget.show();
-    var user;
-    userlist = [
-    {"id":"f1","name":"张三1","portraitUri":""},
-    {"id":"f2","name":"fanwei","portraitUri":""},
-    {"id":"f3","name":"李四3","portraitUri":""}
-    ]
-    userlist.forEach(function(user){
+    
       WebIMWidget.setUserInfoProvider(function(targetId,obj){
-        obj.onSuccess({id:user.id,name:user.name,portraitUri:user.portraitUri});
+        var user;
+        userlist = [
+        {"id":"f1","name":"张三1","portraitUri":""},
+        {"id":"f2","name":"fanwei","portraitUri":""},
+        {"id":"f3","name":"李四3","portraitUri":""}
+        ]
+        
+
+        userlist.forEach(function(item){
+          if(item.id==targetId){
+            user=item;
+          }
+        })
+
+        if(user){
+          obj.onSuccess({id:user.id,name:user.name,portraitUri:user.portraitUri});
+        }
       });
-    });
+
+
 
 
     // WebIMWidget.setConversation(WebIMWidget.EnumConversationType.PRIVATE,"f3","张三");
